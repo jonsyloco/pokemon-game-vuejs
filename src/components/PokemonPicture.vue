6a3f308e0d465b00bb2a6e1c7ccec2347ad8a0a4
jhonatan wagner ocampo <jonsyloco@gmail.com>
2021-12-30 17:02:27 -0500

    comienzo de la aplicacion de pokemon

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="pokemon-container">
+      <img class="hidden-pokemon" :src="imgSrc" alt="">
+      <img v-if="showPokemon" class="fade-in" :src="imgSrc" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+      pokemonId: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    showPokemon: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  computed: {
+    imgSrc() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .pokemon-container {
+      height: 200px;
+  }
+  img {
+    height: 200px;
+    position: absolute;
+    width: 100vw;
+    margin-left: -50vw;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+  }
+  .hidden-pokemon {
+    filter: brightness(0);
+  }
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <h1>Quien es este pokémon?</h1>
+      <pokemon-picture 
+        :pokemonId="4"
+        :showPokemon="true"
+      >
+      </pokemon-picture>
+      <pokemon-option></pokemon-option>
+  </div>
+</template>
+
+<script>
+import pokemonPicture from '../components/PokemonPicture.vue';
+import PokemonOption from '../components/PokemonOption.vue';
+
+export default {
+  name: 'PokemonPage',
+  components: {
+    pokemonPicture,
+    PokemonOption,
+  }
+}
+</script>
+
+<style>
+
+</style>
