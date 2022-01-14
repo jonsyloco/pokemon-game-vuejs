672a1cd7d81b27de3e627d756c77ecd213bd1df3
jonsyloco <jonsyloco@gmail.com>
2022-01-14 18:03:09 -0500

    se agrega opcion de elegir ganador

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="pokemon-container">
-      <img class="hidden-pokemon" :src="imgSrc" alt="">
-      <img v-if="showPokemon" class="fade-in" :src="imgSrc" alt="">
+      <img class="hidden-pokemon" :src="imgSrc" alt="" />
+      <img v-if="showPokemon" class="fade-in" :src="imgSrc" alt="" />
     </div>
   </div>
 </template>
@@ -10,7 +10,7 @@
 <script>
 export default {
   props: {
-      pokemonId: {
+    pokemonId: {
       type: Number,
       required: true,
       default: 1,
@@ -24,27 +24,27 @@ export default {
   computed: {
     imgSrc() {
       return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .pokemon-container {
-      height: 200px;
-  }
-  img {
-    height: 200px;
-    position: absolute;
-    width: 100vw;
-    margin-left: -50vw;
-    user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    -webkit-user-drag: none;
-    -webkit-user-select: none;
-  }
-  .hidden-pokemon {
-    filter: brightness(0);
-  }
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  width: 100vw;
+  margin-left: -50vw;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
 </style>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,25 +1,28 @@
 <template>
   <div>
-      <h1 v-if="!pokemon">Espere por favor...</h1>
-      <div v-else>
-        <h1>Quien es este pokémon?</h1>
-        <pokemon-picture 
-          :pokemonId="pokemon.id"
-          :showPokemon="true"
-        >
-        </pokemon-picture>
-        <pokemon-option :pokemons = "pokemonArr"></pokemon-option>
-      </div>
+    <h1 v-if="!pokemon">Espere por favor...</h1>
+    <div v-else>
+      <h1>Quien es este pokémon?</h1>
+      <pokemon-picture :pokemonId="pokemon.id" :showPokemon="showPokemon">
+      </pokemon-picture>
+      <pokemon-option
+        :pokemons="pokemonArr"
+        @selection-pokemon="chekAnswer($event)"
+      ></pokemon-option>
+
+      <h2>{{ message }}</h2>
+      <button>Nuevo juego</button>
+    </div>
   </div>
 </template>
 
 <script>
-import pokemonPicture from '../components/PokemonPicture.vue';
-import PokemonOption from '../components/PokemonOption.vue';
-import getPokemonOptions from '../helpers/getPokemonOPtions';
+import pokemonPicture from "../components/PokemonPicture.vue";
+import PokemonOption from "../components/PokemonOption.vue";
+import getPokemonOptions from "../helpers/getPokemonOPtions";
 
 export default {
-  name: 'PokemonPage',
+  name: "PokemonPage",
   components: {
     pokemonPicture,
     PokemonOption,
@@ -28,22 +31,31 @@ export default {
     return {
       pokemonArr: [],
       pokemon: null,
-    }
+      showPokemon: false,
+      showAnswer: false,
+      message: "",
+    };
   },
   methods: {
     async mixPokemonArray() {
       this.pokemonArr = await getPokemonOptions();
-      const rndInt = Math.floor( Math.random() * 4);
-      this.pokemon = this.pokemonArr[ rndInt ];
-    }
-
+      const rndInt = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemonArr[rndInt];
+    },
+    chekAnswer(pokemonId) {
+      if (this.pokemon.id == pokemonId) {
+        this.showPokemon = true;
+        this.message = `Correcto ${this.pokemon.name}`;
+      } else {
+        this.message = `Oops, era ${this.pokemon.name}`;
+      }
+    },
   },
   mounted() {
     this.mixPokemonArray();
-  }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
